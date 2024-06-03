a9ab9f15723c254c38bdea9c23a4766f4f4dcb3d
droubarka <droubarka@gmail.com>
2024-06-03 08:05:42 -0400

    Add /tests/new-features.py

---
+++ tests/new-features.py
@@ -0,0 +1,10 @@
+# /tests/new-features.py
+
+def shift_tbytes(data: bytes, table: bytes, shift: int, reverse: bool = False) -> bytes:
+	pass
+
+def shift_talpha(data: str, table: str, shift: int, reverse: bool = False) -> str:
+	pass
+
+def shift_alpha_v2(data: str, shift: int, reverse: bool = False) -> str:
+	pass
