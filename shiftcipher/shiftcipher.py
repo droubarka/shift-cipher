35342d4e6e3485f6146209d605f683be238bad3b
droubarka <droubarka@gmail.com>
2024-06-03 06:14:00 -0400

    Add reverse option to the global_shift function

--- shiftcipher/shiftcipher.py
+++ shiftcipher/shiftcipher.py
@@ -1,6 +1,6 @@
 # /shiftcipher/shiftcipher.py
 
-def global_shift(data: list, table: list, shift: int) -> list:
+def global_shift(data: list, table: list, shift: int, reverse: bool = False) -> list:
 	"""
 	This function performs a circular shift on a list of elements using a given table.
 
@@ -8,10 +8,12 @@ def global_shift(data: list, table: list, shift: int) -> list:
 		data: A list of elements to be shifted.
 		table: A list of elements to use as the table for shifting.
 		shift: An integer representing the number of positions shift.
+		reverse: A boolean flag indicating whether to shift in reverse direction.
 
 	Returns:
 		A list of shifted elements.
 	"""
+	shift = -shift if reverse else shift
 	shifted_data = list( \
 		table[(table.index(element) + shift) % len(table)] for element in data)
 
