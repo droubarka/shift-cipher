e11dc8989f6953d44a36dc361a70a14342b46ca2
Mohamed A. Oubarka <droubarka@gmail.com>
2024-06-04 05:59:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ setup(
 	url='https://github.com/droubarka/shift-cipher.git',
 	description='Shift-Cipher is a traditional encryption method used to encrypt texts.',
 	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
 	classifiers=[
 		'Programming Language :: Python :: 3',
 		'License :: OSI Approved :: MIT License',
 	],
 	keywords=(
-		'cryptography substitution shift cipher'
+		'cryptography substitution shift caesar cipher'
 		'algorithm security cybersecurity'
 		'code encryption decryption key'),
 	license='MIT License',
